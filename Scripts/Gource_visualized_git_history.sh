8880b6369223889c8ba3b525d72a5e50519a362d
Masrkai <118424417+Masrkai@users.noreply.github.com>
2025-12-19 14:42:37 +0200

    Solved Gource Task

---
+++ Scripts/Gource_visualized_git_history.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+gource --auto-skip-seconds 0.1 -s 0.1 --output-ppm-stream - | ffmpeg -y -r 60 -f image2pipe -vcodec ppm -i - -c:v libx264 -preset fast -crf 18 -pix_fmt yuv420p -movflags +faststart movie.mp4
