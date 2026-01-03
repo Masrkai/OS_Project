59740f095fded28efca729d40c238ac258532f9c
Masrkai <118424417+Masrkai@users.noreply.github.com>
2026-01-03 12:27:54 +0200

    simple trailing spaces and updated compile commands

--- Needs looking/test_process_generator.c
+++ Needs looking/test_process_generator.c
@@ -22,25 +22,25 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: %s <msgqid>\n", argv[0]);
         return 1;
     }
-    
+
     int msgqid = atoi(argv[1]);
     Message msg;
-    
+
     printf("Test scheduler started with msgqid=%d...\n", msgqid);
-    
+
     while (1) {
         // Receive message of any type (mtype = 0 means any message)
         if (msgrcv(msgqid, &msg, sizeof(Process), 0, 0) == -1) {
             perror("msgrcv failed");
             return 1;
         }
-        
+
         // Check for termination message
         if (msg.mtype == 2) {
             printf("Received termination message\n");
             break;
         }
-        
+
         // Log received process
         printf("Received process: ID=%d arrival=%d runtime=%d priority=%d (mtype=%ld)\n",
                msg.process.id,
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
                msg.process.priority,
                msg.mtype);
     }
-    
+
     printf("Test scheduler exiting normally\n");
     return 0;
 }
