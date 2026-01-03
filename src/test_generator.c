1831ebf3c26cb007a0ed21cda859dca844b9f3ff
Masrkai <118424417+Masrkai@users.noreply.github.com>
2026-01-03 12:27:26 +0200

    made test generator unit testable

--- src/test_generator.c
+++ src/test_generator.c
@@ -11,26 +11,47 @@ struct processData {
   int id;
 };
 
+// Testable function that generates process data
+// Returns 0 on success, -1 on error
+int generate_processes(FILE *output, int no, unsigned int seed) {
+  if (output == null || no <= 0) {
+    return -1;
+  }
+
+  srand(seed);
+
+  fprintf(output, "#id arrival runtime priority\n");
+
+  struct processData pData;
+  pData.arrivaltime = 1;
+
+  for (int i = 1; i <= no; i++) {
+    pData.id = i;
+    pData.arrivaltime += rand() % (11);
+    pData.runningtime = rand() % (30);
+    pData.priority = rand() % (11);
+    fprintf(output, "%d\t%d\t%d\t%d\n", pData.id, pData.arrivaltime,
+            pData.runningtime, pData.priority);
+  }
+
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   FILE *pFile;
   pFile = fopen("processes.txt", "w");
+  if (pFile == null) {
+    fprintf(stderr, "Error: Could not open processes.txt for writing\n");
+    return 1;
+  }
+
   int no;
-  struct processData pData;
   printf("Please enter the number of processes you want to generate: ");
   scanf("%d", &no);
-  srand(time(null));
-  // fprintf(pFile,"%d\n",no);
-  fprintf(pFile, "#id arrival runtime priority\n");
-  pData.arrivaltime = 1;
-  for (int i = 1; i <= no; i++) {
-    // generate Data Randomly
-    //[min-max] = rand() % (max_number + 1 - minimum_number) + minimum_number
-    pData.id = i;
-    pData.arrivaltime += rand() % (11); // processes arrives in order
-    pData.runningtime = rand() % (30);
-    pData.priority = rand() % (11);
-    fprintf(pFile, "%d\t%d\t%d\t%d\n", pData.id, pData.arrivaltime,
-            pData.runningtime, pData.priority);
-  }
+
+  int result = generate_processes(pFile, no, time(null));
+
   fclose(pFile);
+
+  return result == 0 ? 0 : 1;
 }
