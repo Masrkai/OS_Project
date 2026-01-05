3aec88a6063b8b93abd6e2647588226f8a3335c4
Masrkai <118424417+Masrkai@users.noreply.github.com>
2026-01-05 06:33:57 +0200

    commit for GUI

--- UI/dark.c
+++ UI/dark.c
@@ -1,35 +1,29 @@
 #include <gtk/gtk.h>
-#include "resources/style_embedded.h"
 
 static void activate(GtkApplication *app, gpointer user_data) {
-    GtkWidget *window;
-    GtkWidget *header_bar;
-    GtkCssProvider *css_provider;
+    GtkBuilder *builder = gtk_builder_new_from_file("window.ui");
+    GObject *window_obj = gtk_builder_get_object(builder, "main_window");
+    GtkWidget *window = GTK_WIDGET(window_obj);
+    
+    // Essential: Link window to application
+    gtk_window_set_application(GTK_WINDOW(window), app);
 
-    // Create window
-    window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Elegant Scheduler");
-    gtk_window_set_default_size(GTK_WINDOW(window), 800, 800);
+    GtkCssProvider *css_provider = gtk_css_provider_new();
+    GFile *css_file = g_file_new_for_path("styles.css");
+    gtk_css_provider_load_from_file(css_provider, css_file);
+    g_object_unref(css_file);
 
-    // Create header bar
-    header_bar = gtk_header_bar_new();
-    gtk_header_bar_set_show_title_buttons(GTK_HEADER_BAR(header_bar), TRUE);
-    gtk_window_set_titlebar(GTK_WINDOW(window), header_bar);
-
-    // Apply dark theme CSS from embedded data
-    css_provider = gtk_css_provider_new();
-    gchar *css_string = g_strndup((const gchar *)style_css, style_css_len);
-    gtk_css_provider_load_from_string(css_provider, css_string);
-    g_free(css_string); // cleanup
 
     gtk_style_context_add_provider_for_display(
         gdk_display_get_default(),
         GTK_STYLE_PROVIDER(css_provider),
         GTK_STYLE_PROVIDER_PRIORITY_APPLICATION
     );
-
+    
     gtk_window_present(GTK_WINDOW(window));
+    g_object_unref(builder);
 }
+
 
 int main(int argc, char **argv) {
     GtkApplication *app;
@@ -37,7 +31,7 @@ int main(int argc, char **argv) {
 
     g_setenv("GSK_RENDERER", "opengl", TRUE);
 
-    app = gtk_application_new("com.example.elegantdark", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("com.scheduler.viewer", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
